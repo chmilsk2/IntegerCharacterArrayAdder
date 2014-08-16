bce9483c91c57d436926bc1b737c12c7a7d99aef
Troy Chmieleski <tchmieleski@incubate.co>
2014-08-15 20:21:13 -0500

    test with four integer character arrays

--- IntegerCharArray/ViewController.m
+++ IntegerCharArray/ViewController.m
@@ -18,22 +18,26 @@
 	char characterIntegerArrayOne[] = {'9', '8', '7'};
 	char characterIntegerArrayTwo[] = {'1', '3'};
 	char characterIntegerArrayThree[] = {'1', '0'};
+	char characterIntegerArrayFour[] = {'5'};
 	
 	int characterIntegerArrayOneCount = sizeof(characterIntegerArrayOne)/sizeof(char);
 	int characterIntegerArrayTwoCount = sizeof(characterIntegerArrayTwo)/sizeof(char);
 	int characterIntegerArrayThreeCount = sizeof(characterIntegerArrayThree)/sizeof(char);
+	int characterIntegerArrayFourCount = sizeof(characterIntegerArrayFour)/sizeof(char);
 	
-	int numberOfCharacterArrays = 3;
+	int numberOfCharacterArrays = 4;
 	
 	CharacterArray characterArrayOne = {.count = characterIntegerArrayOneCount, .array = characterIntegerArrayOne};
 	CharacterArray characterArrayTwo = {.count = characterIntegerArrayTwoCount, .array = characterIntegerArrayTwo};
 	CharacterArray characterArrayThree = {.count = characterIntegerArrayThreeCount, .array = characterIntegerArrayThree};
+	CharacterArray characterArrayFour = {.count = characterIntegerArrayFourCount, .array = characterIntegerArrayFour};
 	
-	CharacterArray characterSumArray = addIntegersRepresentedAsCharacterArrays(numberOfCharacterArrays, characterArrayOne, characterArrayTwo, characterArrayThree);
+	CharacterArray characterSumArray = addIntegersRepresentedAsCharacterArrays(numberOfCharacterArrays, characterArrayOne, characterArrayTwo, characterArrayThree, characterArrayFour);
 	
 	printCharacterArray(characterArrayOne);
 	printCharacterArray(characterArrayTwo);
 	printCharacterArray(characterArrayThree);
+	printCharacterArray(characterArrayFour);
 	printCharacterArray(characterSumArray);
 }
 
